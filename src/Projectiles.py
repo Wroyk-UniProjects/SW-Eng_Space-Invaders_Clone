d52ab33b527de558d8819226df4f43694e893baa
Anscit06 <Schwmailandreas@gmail.com>
2021-11-24 00:12:36 +0100

    SESI-8 Projectiles started

---
+++ src/Projectiles.py
@@ -0,0 +1,13 @@
+#Attributes: Coordinates(x,y), velocity v, life hp,
+class projectiles(object):
+    def __init__(self):
+        self.display()
+        self.startMoving()
+        self.detectHitbox()
+    def __del__(self):
+        self.stopDisplay()
+        self.
+    def startMoving(self):
+
+    def detectHitbox(self):
+        self.decrementHp()
