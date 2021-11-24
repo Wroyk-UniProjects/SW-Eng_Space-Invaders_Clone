884bebd5ca19ec80c032c2beea1a1e1ad2b0e1cc
Rudolf Baun <Rudolf.Baun@rudolfbaun.de>
2021-11-24 14:05:31 +0100

    no test

--- tests/unitTests/test_main.py
+++
@@ -1,9 +0,0 @@
-import main
-
-
-class TestMain:
-    def test_print_hi1(self):
-        assert main.print_hi("hallo1") == 4
-
-    def test_print_hi2(self):
-        assert main.print_hi("hallo2") == 5
