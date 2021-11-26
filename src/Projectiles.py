2ac0d67b5654a038cb5617a91e3444eae85f3f9a
Anscit06 <Schwmailandreas@gmail.com>
2021-11-26 17:25:57 +0100

    SESI-8 i guess done, though i don't know how to test it

--- src/Projectiles.py
+++ src/Projectiles.py
@@ -1,13 +1,17 @@
-#Attributes: Coordinates(x,y), velocity v, life hp,
-class projectiles(object):
-    def __init__(self):
-        self.display()
-        self.startMoving()
-        self.detectHitbox()
-    def __del__(self):
-        self.stopDisplay()
-        self.
-    def startMoving(self):
+import hitbox.py
 
-    def detectHitbox(self):
-        self.decrementHp()
+class projectiles(object, Hitbox):
+    direction = 0   # 10 is up, -10 is down. Could be implemented differently...
+
+    # Spawn with coordinates, direction(up for player, down for enemy) and hitbox
+    def spawn(self, x, y, size, direc):
+        self.__init__(x, y, size)
+        self.start_moving(direc)
+
+    def start_moving(self, direc):
+        self.direction = direc
+        while True:
+            self.set_hitbox_position(self.pos_x, self.pos_y + self.direction)
+            if self.is_touching():
+                self.__del__()
+                # Also delete hit object
