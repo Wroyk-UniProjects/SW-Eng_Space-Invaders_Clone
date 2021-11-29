65f8932ccde27a808d1d5dc63f16bd55c7b3f826
robertfeo <rofeit00@hs-esslingen.de>
2021-11-29 14:52:13 +0100

    SESI-15_hitbox - collision detection is working properly now.

--- src/hitbox.py
+++ src/hitbox.py
@@ -1,81 +1,43 @@
-import numpy as np
-import math
 
-
+# Hitbox ( position_x, position_y, width, length )
 class Hitbox:
     # X and Y positions
     pos_x = 0
     pos_y = 0
 
-    # it's a zero square.
-    size = 0
+    # Hitbox sizes
+    width = 0
+    length = 0
 
     # class initialisation
-    def __init__(self, pos_x, pos_y, size):
+    def __init__(self, pos_x, pos_y, l, w):
         self.pos_x = pos_x
         self.pos_y = pos_y
-        self.size = size
+        self.length = l
+        self.width = w
 
-    def set_pos_x(self, posx):
-        self.pos_x = posx
+    def set_pos_x(self, x):
+        self.pos_x = x
 
-    def set_pos_y(self, posy):
-        self.pos_y = posy
+    def set_pos_y(self, y):
+        self.pos_y = y
 
-    def set_hitbox_position(self, posx, posy):
-        self.pos_x = posx
-        self.pos_y = posy
+    def set_hitbox_position(self, x, y):
+        self.pos_x = x
+        self.pos_y = y
+
+    def set_hitbox_size(self, length, width):
+        self.width = width
+        self.length = length
 
     def get_hitbox_position(self):
-        return self.pos_x, self.pos_y
-
-    def set_hitbox_size(self, size):
-        self.size = size
+        return self.pos_x, self.pos_y  # returns array of (x, y)
 
     def get_size_hitbox(self):
-        return self.size
+        return self.width, self.length  # returns array of (width, length)
 
-    # almost works, still buggy though
     def is_touching(self, other_obj: "hitbox"):
-        # this is a comparison between a vector normalization and the sum of objects sizes
-        if np.linalg.norm(np.array([self.pos_x, self.pos_y]) - np.array([other_obj.pos_x, other_obj.pos_y])) <= (
-                self.size + other_obj.size):
+        if (self.pos_x < other_obj.pos_x + other_obj.width) and (self.pos_x + self.width > other_obj.pos_x) and (self.pos_y < other_obj.pos_y + other_obj.length) and (self.length + self.pos_y > other_obj.pos_y):
             return True
         else:
             return False
-
-##################################################################################################
-
-    def magnitude(self, v):
-        return math.sqrt(sum(v[i] * v[i] for i in range(len(v))))
-
-    def add(self, u, v):
-        return [u[i] + v[i] for i in range(len(u))]
-
-    def sub(self, u, v):
-        return [u[i] - v[i] for i in range(len(u))]
-
-    def normalize(self, v):
-        return [v[i] / self.magnitude(v) for i in range(len(v))]
-
-    def check_collide(self, other_obj: "hitbox"):
-        vec_h1 = np.array([self.pos_x, self.pos_y])
-        vec_h2 = np.array([other_obj.pos_x, other_obj.pos_y])
-        print(vec_h1)
-        print(vec_h2)
-
-        print("\n")
-
-        print(np.array([self.pos_x, self.pos_y]) - np.array([other_obj.pos_x, other_obj.pos_y]))
-        print(str(round(np.linalg.norm(np.array([self.pos_x, self.pos_y]) - np.array([other_obj.pos_x, other_obj.pos_y])),0)))
-        print(self.size + other_obj.size)
-
-        if round(np.linalg.norm(np.array([self.pos_x, self.pos_y]) - np.array([other_obj.pos_x, other_obj.pos_y])),0) < (
-                self.size + other_obj.size):
-            print("true")
-        else:
-            print("false")
-
-        #norm = self.normalize(vec_h1,vec_h2)
-
-        #print("Normalization: " + str(norm))
--- tests/unitTests/test_hitbox.py
+++ tests/unitTests/test_hitbox.py
@@ -1,17 +1,15 @@
 from src import hitbox
 
 if __name__ == '__main__':
-    # hitbox nr 1
-    hitbox1 = hitbox.Hitbox(5, 5, 5)
+    # hitbox ( position_x, position_y, width, length )
+    hitbox1 = hitbox.Hitbox(0, 0, 5, 5)
     print("\nSize of Hitbox 1: " + str(hitbox1.get_size_hitbox()))
     print("Position of Hitbox 1: " + str(hitbox1.get_hitbox_position()))
 
-    # hitbox nr 2
-    hitbox2 = hitbox.Hitbox(10, 10, 5)
+    hitbox2 = hitbox.Hitbox(4, 4, 5, 5)
     print("\nSize of Hitbox 2: " + str(hitbox2.get_size_hitbox()))
     print("Position of Hitbox 2: " + str(hitbox2.get_hitbox_position()))
 
-    #check collision
+    # check collision
     collided = hitbox2.is_touching(hitbox1)
-    print("\nthey are colliding: " + str(collided))
-    hitbox2.check_collide(hitbox1)
+    print("\nHitboxes colliding: " + str(collided))
