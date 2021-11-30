a0eb9fe3ba14c61fcf28ff4e4f07aea6b9e34113
Anscit06 <Schwmailandreas@gmail.com>
2021-11-30 18:10:43 +0100

    SESI-8 projectiles updated, update() new position calculation updated

--- src/Projectiles.py
+++ src/Projectiles.py
@@ -9,14 +9,13 @@ class Projectiles(GameObject, Hitbox):
 
     def __init__(self, x, y, length, width, velo):
         super(Projectiles, self).__init__(x, y, length, width)
-        self.update(velo)
+        self.velocity = velo
 
     def draw(self):
         pass
 
-    def update(self, velo):
-        self.velocity = velo
-        self.set_hitbox_position(self.pos_x, self.pos_y + self.velocity)
+    def update(self, dt):
+        self.set_hitbox_position(self.pos_x, self.pos_y + self.velocity * dt)
         if self.is_touching():
             self.__del__()
             # Also delete hit object
