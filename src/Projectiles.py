c504d60c062fa87046a5f406b0789a03e1d5fb32
Anscit06 <Schwmailandreas@gmail.com>
2021-11-30 15:11:54 +0100

    SESI-8 projectiles updated, missing draw and update implementation

--- src/Projectiles.py
+++ src/Projectiles.py
@@ -1,12 +1,15 @@
 from hitbox import Hitbox
+from gameobject import GameObject
 
+def spawn(x, y, length, width, direc):
+    return Projectiles(x, y, length, width, direc)
 
-class Projectiles(Hitbox):
+class Projectiles(GameObject, Hitbox):
     direction = 0   # 10 is up, -10 is down. Could be implemented differently...
 
     # Spawn with coordinates, direction(up for player, down for enemy) and hitbox
-    def spawn(self, x, y, size, direc):
-        self.__init__(x, y, size)
+    def __init__(self, x, y, length, width, direc):
+        super(Projectiles, self).__init__(x, y, length, width)
         self.start_moving(direc)
 
     def start_moving(self, direc):
@@ -16,3 +19,9 @@ class Projectiles(Hitbox):
             if self.is_touching():
                 self.__del__()
                 # Also delete hit object
+
+    def draw(self):
+        pass
+
+    def update(self, dt):
+        pass
