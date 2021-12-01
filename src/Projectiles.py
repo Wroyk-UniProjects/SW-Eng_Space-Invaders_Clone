fa5eb4c32cb54a84ab0f4a9375dbfeb561401ebb
Rudolf Baun <Rudolf.Baun@rudolfbaun.de>
2021-12-01 19:05:50 +0100

    smale adjustment

--- src/Projectiles.py
+++ src/Projectiles.py
@@ -1,14 +1,17 @@
 from hitbox import Hitbox
 from gameobject import GameObject
 
+
 def spawn(x, y, length, width, velo):
     return Projectiles(x, y, length, width, velo)
+
 
 class Projectiles(GameObject, Hitbox):
     velocity = 0
 
     def __init__(self, x, y, length, width, velo):
         super(Projectiles, self).__init__(x, y, length, width)
+        self.sprite = None
         self.velocity = velo
 
     def draw(self):
@@ -17,5 +20,5 @@ class Projectiles(GameObject, Hitbox):
     def update(self, dt):
         self.set_hitbox_position(self.pos_x, self.pos_y + self.velocity * dt)
         if self.is_touching(other_obj="hitbox"):
-            self.__del__()
-
+            # self.__del__()
+            pass
