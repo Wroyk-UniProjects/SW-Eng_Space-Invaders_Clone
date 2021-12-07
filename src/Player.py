198f4cb80cd787a78fd1fec26f8ef834dd33880c
Julia Takagi <takagi002@gannon.edu>
2021-12-07 11:11:40 +0100

    accidentally added on key press to this branch instead of my own - will change after this commit

--- src/Player.py
+++ src/Player.py
@@ -1,5 +1,6 @@
 import pyglet
 from pyglet.sprite import Sprite
+from pyglet.window import key
 
 from Projectiles import Projectiles
 from gameobject import GameObject
@@ -11,6 +12,8 @@ class Player (GameObject):
         self.startx = startx
         self.starty = starty
         self.icon = icon
+        self.velocityLeft = -1
+        self.velocityRight = 1
 
         self.hitbox = Hitbox(self.startx, self.starty, 100, 100)
         self.projectile = Projectiles(self.startx, self.starty, 100, 100, 20)
@@ -21,14 +24,25 @@ class Player (GameObject):
 
     #movement functions
     def moveright(self):
-        self.startx += 1
+        self.rightMovementKeyD = key.D
+        self.rightMovementKeyRight = key.RIGHT
+
+
+    def on_key_press(self, symbol, modifiers):
+        if symbol is key.D or key.RIGHT:
+            self.moveright()
+        elif symbol is key.A or key.LEFT:
+            self.moveleft()
+        elif symbol is key.SPACE:
+            self.shootprojectile()
+
 
     def moveleft(self):
-        self.startx -= 1
+        self.leftMovementKeyA = key.A
+        self.leftMovementKeyLeft = key.LEFT
 
     #shooting
     def shootprojectile(self):
-        #do something
         self.projectile.spawn()
 
     #from gameobject
@@ -37,3 +51,4 @@ class Player (GameObject):
 
     def update(self, dt):
         pass
+
