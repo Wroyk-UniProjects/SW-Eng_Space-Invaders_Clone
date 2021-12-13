1d68d0a099be0f05e72c361fabcd054bf66fcc94
Rudolf Baun <Rudolf.Baun@rudolfbaun.de>
2021-12-13 22:07:08 +0100

    SESI-45 they can shoot

--- src/enemy.py
+++ src/enemy.py
@@ -1,46 +1,53 @@
+import random
+
 import pyglet
 
+import gameobject
+import projectile
 from gameobject import GameObject
 import hitbox
-
 
 distanceMoveInFront = 10
 speedIncrementMovinginFront = 10
 
+shoot_cooldown = 0
+
+
 class Enemy(GameObject):
+    cooldown_time = 4
 
-    #Constructor
-    def __init__(self, x, y, startSpeed, image, positionInMesh):
+    # Constructor
+    def __init__(self, x, y, startSpeed, image, positionInMesh, batch):
 
-        #Location
+        # Location
         self.x = x
         self.y = y
 
-        #EnemySpeed at the start
+        # EnemySpeed at the start
         self.speed = startSpeed
 
-        #Sprite setup
+        self.batch = batch
+        # Sprite setup
         image = pyglet.image.load(image)
-        self.sprite = pyglet.sprite.Sprite(image, x=self.x, y=self.y)
+        self.sprite = pyglet.sprite.Sprite(image, x=self.x, y=self.y,group=gameobject.GAMEOBJECTS, batch=self.batch)
         self.sprite.update(scale_x=0.05, scale_y=0.05)
 
-        #Size of enemy
+        # Size of enemy
         self.enemyWidth = self.sprite.width
         self.enemyHeight = self.sprite.height
         self.direction = False
 
-        #We need this Parameter, to synchronise the movement of multiple Enemies in a Mesh
+        # We need this Parameter, to synchronise the movement of multiple Enemies in a Mesh
         self.positionInMesh = positionInMesh
 
         self.rightBoorder = self.enemyWidth * 10 - self.enemyWidth + self.enemyWidth * self.positionInMesh
-        self.leftBoorder =  40 + self.enemyWidth * self.positionInMesh
+        self.leftBoorder = 40 + self.enemyWidth * self.positionInMesh
 
-        #is the Enemy alive?
+        # is the Enemy alive?
         self.active = True
 
-        #create hitbox for the enemy
-        self.hitbox = hitbox.Hitbox(self.x, self.y, self.enemyWidth, self.enemyHeight)
-
+        # create hitbox for the enemy
+        self.hitbox = hitbox.Hitbox(self.x, self.y, self.enemyWidth, self.enemyHeight, hitbox.HitMask.ENEMY)
 
     def getEnemySpeed(self):
         return self.speed
@@ -54,12 +61,12 @@ class Enemy(GameObject):
     def getEnemyY(self):
         return self.y
 
-    def update(self,dt):
-        if(self.active):
-            if(self.direction):
+    def update(self, dt):
+        if self.active:
+            if (self.direction):
                 self.x += self.speed * dt
                 self.sprite.update(x=self.x)
-                if(self.x >= self.rightBoorder):
+                if (self.x >= self.rightBoorder):
                     self.direction = False
                     self.y = self.y - distanceMoveInFront
                     self.speed = self.speed + speedIncrementMovinginFront
@@ -73,14 +80,18 @@ class Enemy(GameObject):
                     self.speed = self.speed + speedIncrementMovinginFront
                     self.sprite.update(y=self.y - distanceMoveInFront)
 
-                if(self.y <= 200):
+                if (self.y <= 200):
                     self.enemyDie()
 
-    def draw(self):
-        if(self.active):
-            self.sprite.draw()
+            global shoot_cooldown
+            if random.randrange(0, 18) == self.positionInMesh and shoot_cooldown < 0:
+                #print("zt")
 
-    #deleting all the attributes. There is no way for an object to delete itslelf
+                shoot_cooldown = self.cooldown_time
+                projectile.spawn(self.x + self.enemyWidth / 2, self.y + self.enemyHeight / 2, hitbox.HitMask.PLAYER, projectile.Direction.DOWN, self.batch)
+
+
+    # deleting all the attributes. There is no way for an object to delete itslelf
     def enemyDie(self):
         self.active = False
         del self.sprite
@@ -96,34 +107,36 @@ class Enemy(GameObject):
     def on_collision(self):
         self.enemyDie()
 
-#Mesh creates multiple Enemies
-#maybe make them smaller?!
+
+# Mesh creates multiple Enemies
+# maybe make them smaller?!
 
 class EnemyMesh:
 
-
     # Three rows of Enemies right now. Enemies are still a bit big but i will channge them
     # Movement still not working right.
-    def __init__(self, enemyCount):
+    def __init__(self, enemyCount, batch):
 
-        #get enemie height and width
-        pseudoEnemie = Enemy(0, 0, 0, '../assets/enemy_updated.png', 0)
+        # get enemie height and width
+        pseudoEnemie = Enemy(0, 0, 0, '../assets/enemy_updated.png', 0, batch)
         enemieWidth = pseudoEnemie.sprite.width
         enemieHeight = pseudoEnemie.sprite.height
         hitbox.debug_hitboxs.remove(pseudoEnemie.hitbox)
         del pseudoEnemie
 
-
-
         self.enemies = []
         for i in range(enemyCount):
-            self.enemies.append(Enemy(i * enemieWidth + 200, 620, 50, '../assets/enemy_updated.png', i))
-            self.enemies.append(Enemy(i * enemieWidth + 200, 620 - enemieHeight, 50, '../assets/enemy_updated.png', i))
-            self.enemies.append(Enemy(i * enemieWidth + 200, 620 - enemieHeight * 2, 50, '../assets/enemy_updated.png', i))
+            self.enemies.append(Enemy(i * enemieWidth + 200, 620, 50, '../assets/enemy_updated.png', i, batch))
+            self.enemies.append(Enemy(i * enemieWidth + 200, 620 - enemieHeight, 50, '../assets/enemy_updated.png', i, batch))
+            self.enemies.append(
+                Enemy(i * enemieWidth + 200, 620 - enemieHeight * 2, 50, '../assets/enemy_updated.png', i, batch))
 
     def getEnemyMesh(self):
         return self.enemies
 
     def update(self, dt):
         for enemy in self.enemies:
-            enemy.update(dt)
+            if not enemy.active:
+                del enemy
+            else:
+                enemy.update(dt)
--- src/gameobject.py
+++ src/gameobject.py
@@ -8,6 +8,7 @@ GAMEOBJECTS = pyglet.graphics.OrderedGroup(1)
 PROJECTILES = pyglet.graphics.OrderedGroup(2)
 UI = pyglet.graphics.OrderedGroup(3)
 
+game_objects = []
 
 class GameObject(ABC):
     @abstractmethod
@@ -16,3 +17,4 @@ class GameObject(ABC):
 
     def __init__(self, batch):
         self.batch = batch
+        #game_objects.append(self)
