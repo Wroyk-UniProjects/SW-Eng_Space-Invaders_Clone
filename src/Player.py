c1406413f9d4e1143c9768a78a5523a5855d9180
Rudolf Baun <Rudolf.Baun@rudolfbaun.de>
2021-12-13 12:03:33 +0100

    SESI_37 reapplying movement fix and screen wraping
    I fucked up on rebase hat to redo it. Sorry

--- src/Player.py
+++ src/Player.py
@@ -23,27 +23,27 @@ class Player (GameObject):
 
     #movement functions
     def moveright(self):
-        self.velocity = 1
+        self.velocity = 400
 
     def moveleft(self):
-        self.velocity = -1
+        self.velocity = -400
 
     # shooting
     def shootprojectile(self):
         self.projectile.spawn()
 
     def on_key_press(self, symbol, modifiers):
-        if symbol is key.D or key.RIGHT:
+        if symbol is key.D or symbol is key.RIGHT:
             self.moveright()
-        elif symbol is key.A or key.LEFT:
+        elif symbol is key.A or symbol is key.LEFT:
             self.moveleft()
         elif symbol is key.SPACE:
             self.shootprojectile()
 
     def on_key_release(self, symbol, modifier):
-        if symbol is key.D or key.RIGHT:
+        if symbol is key.D or symbol is key.RIGHT:
             self.velocity = 0
-        elif symbol is key.A or key.LEFT:
+        elif symbol is key.A or symbol is key.LEFT:
             self.velocity = 0
 
 
@@ -52,7 +52,11 @@ class Player (GameObject):
         self.sprite.draw()
 
     def update(self, dt):
-        self.startx += self.startx*self.velocity*dt
+        self.startx += self.velocity*dt
+        if self.startx <= -self.sprite.width:
+            self.startx = 1280-self.sprite.width
+        elif self.startx >= 1280:
+            self.startx = 0
         self.sprite.x = self.startx
 
 
